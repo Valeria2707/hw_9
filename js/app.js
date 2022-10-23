8309599709ceac0191679916d6144dd2112b82ea
Valeria Zavadetska <Valeria.zavadetska@gmail.com>
2022-10-23 16:09:15 +0300

    create function

--- js/app.js
+++ js/app.js
@@ -27,40 +27,60 @@ const EXCHANGE = 41;
 const DISCOUNT_STARTS_FROM = 100000;
 const DISCOUNT_STR = 10;
 const DISCOUNT = DISCOUNT_STR / 100;
+let productNumber;
+let selectedProduct;
+let amount;
+let finalPrice;
 
 function showProducts() {
   // Show products
   for (let i = 0; i < products.length; i++) {
-    console.log(`#${(i+1)} Name: ${products[i].name} \nPrice: ${products[i].price}`);
+    console.log(`#${(i + 1)} Name: ${products[i].name} \nPrice: ${products[i].price}`);
   }
 }
 
-// Get product number
-let productNumber;
-do {
-  productNumber = prompt('Enter the product you wanna buy:');
-  productNumber--;
-} while(productNumber < 0 || productNumber > products.length - 1 || isNaN(productNumber));
-
-const selectedProduct = products[productNumber];
-
-// Get amount of products to be bought
-let amount;
-do {
-  amount = parseInt(prompt('Enter the amount of products you wanna buy:'));
-} while(amount <= 0 || isNaN(amount));
-
-// Calculate the initial price
-const finalPrice = selectedProduct.price * amount;
-
-console.log('The price is $' + finalPrice);
-
-// Check if discount needed
-if (finalPrice * EXCHANGE > DISCOUNT_STARTS_FROM) {
-  const superFinalPrice = finalPrice - finalPrice * DISCOUNT;
-  
-  console.log(`Congrats! You got discount ${DISCOUNT_STR}%:`);
-  console.log('Please pay $' + superFinalPrice);
+function getProductNumber() {
+  // Get product number
+  do {
+    productNumber = prompt('Enter the product you wanna buy:');
+    productNumber--;
+  } while (productNumber < 0 || productNumber > products.length - 1 || isNaN(productNumber));
+  selectedProduct = products[productNumber];
 }
 
-showProducts();
+function getAmountProducts() {
+  // Get amount of products to be bought
+  do {
+    amount = parseInt(prompt('Enter the amount of products you wanna buy:'));
+  } while (amount <= 0 || isNaN(amount));
+}
+
+function calculateInitialPrice() {
+  // Calculate the initial price
+  finalPrice = selectedProduct.price * amount;
+  console.log('The price is $' + finalPrice);
+}
+
+function checkDiscountNeeded() {
+  // Check if discount needed
+  if (finalPrice * EXCHANGE > DISCOUNT_STARTS_FROM) {
+    const superFinalPrice = finalPrice - finalPrice * DISCOUNT;
+    console.log(`Congrats! You got discount ${DISCOUNT_STR}%:`);
+    console.log('Please pay $' + superFinalPrice);
+  }
+}
+
+function shop() {
+  showProducts();
+  getProductNumber();
+  getAmountProducts();
+  calculateInitialPrice();
+  checkDiscountNeeded();
+}
+
+shop();
+
+
+
+
+
